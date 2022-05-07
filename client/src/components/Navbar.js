24fb85063d127f009bd087ec8d3c1ecc0f22c9cf
iSaklov <ivansaklov@gmail.com>
2022-05-07 21:37:18 +0200

    reload

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -15,6 +15,7 @@ export const Navbar = () => {
 	return (
 		<nav>
 			<div className="nav-wrapper">
+				{/* https://www.wildcodeschool.com/fr-FR */}
 				<a href="/" className="brand-logo center">
 					<img src="https://www.wildcodeschool.com/assets/logo_main-e4f3f744c8e717f1b7df3858dce55a86c63d4766d5d9a7f454250145f097c2fe.png" alt="Wild Code School logo" style={{maxWidth: "96px"}}/>
 				</a>
